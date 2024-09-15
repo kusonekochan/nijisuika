f015bef3c701f38c5f5397472474762f054d3667
anycolor5032 <chatgptsub@keyvis.jp>
2024-09-15 16:40:07 +0900

    Resolve merge conflict in db.js

--- db.js
+++ db.js
@@ -5,13 +5,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-<<<<<<< HEAD
-    rejectUnauthorized: false
-  }
-=======
     rejectUnauthorized: false,
   },
->>>>>>> 8d6451687319de50b94334db384cc9912f7a90fd
 });
 
 module.exports = {
